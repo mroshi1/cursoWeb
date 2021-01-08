9400a82a1ed7d4d97f6dbed336b02ebeead93167
mroshi1 <mroshi1@github.com>
2021-01-08 15:59:46 -0300

    Dia 08-01

---
+++ Fundamentos/objetos.js
@@ -0,0 +1,11 @@
+const prod1 = {}
+prod1.nome = "nome do produto"
+prod1.preco = 900
+prod1.desc = "Teste"
+console.log(prod1)
+
+const prod2 = {
+    nome:'Camisa',
+    preco: 40
+}
+console.log("Produto 2:",prod2)
