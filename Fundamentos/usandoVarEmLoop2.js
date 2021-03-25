df8c9802b787887c938fe6bf5cca3633fba24575
mroshi1 <mroshi1@github.com>
2021-03-25 20:35:46 -0300

    Feito dia 25-03-21

--- Fundamentos/usandoVarEmLoop2.js
+++ Fundamentos/usandoVarEmLoop2.js
@@ -1,6 +1,9 @@
 const funcs = []
-    for (var i = 0; i < 10; i++){
-        funcs.pushfunction() {
-            console.log(i)
-        }
-    }
+for (let i = 0; i < 10; i++){
+    funcs.push(function(){
+        console.log(i)
+    })
+}
+
+funcs[2]()
+funcs[8]()
